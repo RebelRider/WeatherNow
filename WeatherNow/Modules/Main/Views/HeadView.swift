fa68a989c4652e31aaeb1e4108cb38c62a9063d9
Kirill Smirnov <kirill.smirnov@orioninc.com>
2022-03-02 08:31:50 +0300

    HeadView separation

--- WeatherNow/Modules/Main/Views/HeadView.swift
+++ WeatherNow/Modules/Main/Views/HeadView.swift
@@ -1,115 +1,128 @@
-//import UIKit
-//
-////MARK: - head container elements
-//var isExpanded = true
-//
-//class headView: UIView {
-//
-//
-//private let cityLabel: UILabelWithPadding = {
-//    let lblc = UILabelWithPadding()
-//    lblc.text = "New York"
-//    lblc.font = lblc.font.withSize(28)
-//    lblc.setSize(width: 266, height: 44)
-//    lblc.backgroundColor = .brown
-//    return lblc
-//}()
-//
-//private let locationMenuButton: UIButton = {
-//    let button = UIButton()
-//    button.setImage(UIImage(systemName: "line.3.horizontal")?
-//                        .withTintColor(.green, renderingMode: .alwaysOriginal)
-//                        .resized(to: CGSize(width: 25, height: 25)), for: .normal)
-//    button.setImage(UIImage(systemName: "line.3.horizontal.decrease")?
-//                        .withTintColor(.green, renderingMode: .alwaysOriginal)
-//                        .resized(to: CGSize(width: 25, height: 25)), for: UIControl.State.highlighted)
-//    //button.addTarget(self, action: #selector(showLocationMenuController), for: .touchUpInside)
-//    
-//    return button
-//}()
-//
-//private let temperatureLabel: UILabelWithPadding = {
-//    let lblt = UILabelWithPadding()
-//    lblt.text = "+25k"
-//    lblt.font = lblt.font.withSize(33)
-//    lblt.setSize(width: 111, height: 44)
-//    return lblt
-//}()
-//
-//private let cloudyImageContainer: UIImageView = {
-//    var view = UIImageView()
-//    let img = UIImage(systemName: "cloud.drizzle.fill")!
-//                .withTintColor(.white, renderingMode: .alwaysOriginal)
-//                .resized(to: CGSize(width: 66, height: 77))
-//    let imgView = UIImageView(image: img)
-//    imgView.frame = CGRect(x: 1, y: 1, width: 66, height: 88)
-//    view.addSubview(imgView)
-//    return view
-//}()
-//
-//
-//
-//private let windPressureHumidityContainer: UIView = {
-//   let wphc = UIView()
-//    
-//    let wind = UILabel()
-//    let pressure = UILabel()
-//    let humid = UILabel()
-//    
-//    wind.addLeading(image: UIImage(systemName: "wind") ?? UIImage(), text: "10 m/s, NW")
-//    pressure.addLeading(image: UIImage(systemName: "timer") ?? UIImage(), text: "701 mPa")
-//    humid.addLeading(image: UIImage(systemName: "humidity") ?? UIImage(), text: "77%")
-//    
-//    let stackV1 = UIStackView(arrangedSubviews: [wind, pressure, humid])
-//    stackV1.axis = .vertical
-//    stackV1.distribution = .fillProportionally
-//    stackV1.spacing = 17
-//    stackV1.backgroundColor = .systemBlue
-//    
-//    wphc.addSubview(stackV1)
-//    stackV1.anchr(top: wphc.topAnchor,
-//                  paddingTop: 0,
-//                  paddingLeft: 45,
-//                  paddingRight: 25)
-//    return wphc
-//}()
-//
-//
-//    //MARK: - days scroll view
-//    private let daysForecastStack: UIStackView = {
-//        let dfc = UIStackView()
-//        dfc.axis = .horizontal
-//        dfc.spacing = 5
-//        dfc.backgroundColor = .systemTeal
-//        return dfc
-//    }()
-//    
-//    private let daysForecastScroll: UIScrollView = {
-//        let dfs = UIScrollView()
-//        dfs.backgroundColor = .green
-//        return dfs
-//    }()
-//
-//    private let daysForecast: UIView = {
-//        let df = UIView()
-//        df.backgroundColor = .red
-//        return df
-//    }()
-//    
-//    let tffff: UILabel = {
-//       let t = UILabel()
-//        t.text = "ttttt fff xcc "
-//        t.font = t.font.withSize(33)
-//        t.setSize(width: 111, height: 44)
-//        return t
-//    }()
-//    
-//    let tffddf: UILabel = {
-//       let t = UILabel()
-//        t.text = "ttccadsfgagatt "
-//        t.font = t.font.withSize(33)
-//        t.setSize(width: 111, height: 44)
-//        return t
-//    }()
-//
-//}
+import UIKit
+
+class HeadView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - head container elements
+    private let isExpanded = true
+    
+    private lazy var headView: UIView = {
+        let hv = UIView()
+        hv.backgroundColor = .green
+        return hv
+    }()
+    
+    let cityLabel: UILabelWithPadding = {
+        let lblc = UILabelWithPadding()
+        lblc.text = "New York"
+        lblc.font = lblc.font.withSize(28)
+        lblc.setSize(width: 266, height: 44)
+        lblc.backgroundColor = .brown
+        return lblc
+    }()
+    
+    let locationMenuButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "line.3.horizontal")?
+                            .withTintColor(.green, renderingMode: .alwaysOriginal)
+                            .resized(to: CGSize(width: 25, height: 25)), for: .normal)
+        button.setImage(UIImage(systemName: "line.3.horizontal.decrease")?
+                            .withTintColor(.green, renderingMode: .alwaysOriginal)
+                            .resized(to: CGSize(width: 25, height: 25)), for: UIControl.State.highlighted)
+        //button.addTarget(self, action: #selector(showLocationMenuController), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    let temperatureLabel: UILabelWithPadding = {
+        let lblt = UILabelWithPadding()
+        lblt.text = "+25k"
+        lblt.font = lblt.font.withSize(33)
+        lblt.setSize(width: 111, height: 44)
+        return lblt
+    }()
+    
+    let cloudyImageContainer: UIImageView = {
+        var view = UIImageView()
+        let img = UIImage(systemName: "cloud.drizzle.fill")!
+            .withTintColor(.white, renderingMode: .alwaysOriginal)
+            .resized(to: CGSize(width: 66, height: 66))
+        let imgView = UIImageView(image: img)
+        view.addSubview(imgView)
+        return view
+    }()
+    
+    
+    //MARK: - Wind Pressure Humidity Container
+    let windPressureHumidityContainer: UIView = {
+        let wphc = UIView()
+        
+        let wind = UILabel()
+        let pressure = UILabel()
+        let humid = UILabel()
+        
+        wind.addLeading(image: UIImage(systemName: "wind") ?? UIImage(), text: "10 m/s, NW")
+        pressure.addLeading(image: UIImage(systemName: "timer") ?? UIImage(), text: "701 mPa")
+        humid.addLeading(image: UIImage(systemName: "humidity") ?? UIImage(), text: "77%")
+        
+        let stackV1 = UIStackView(arrangedSubviews: [wind, pressure, humid])
+        stackV1.axis = .vertical
+        stackV1.distribution = .fillProportionally
+        stackV1.spacing = 17
+        stackV1.backgroundColor = .systemBlue
+        
+        wphc.addSubview(stackV1)
+        stackV1.anchr(top: wphc.topAnchor,
+                      paddingTop: 0,
+                      paddingLeft: 45,
+                      paddingRight: 25)
+        return wphc
+    }()
+    
+    //MARK: - Days Forecast Container
+    let daysForecastContainer: UIView = {
+        let scrollView = UIScrollView()
+        let daysForecast = UIView()
+        
+        let aa = UILabel()
+        let bb = UILabel()
+        let cc = UILabel()
+        let dd = UILabel()
+        let ee = UILabel()
+                
+        aa.addLeading(image: UIImage(systemName: "car") ?? UIImage(), text: "Maondaasada")
+        bb.addLeading(image: UIImage(systemName: "bus") ?? UIImage(), text: "wednes")
+        cc.addLeading(image: UIImage(systemName: "humidity") ?? UIImage(), text: "sunDdyaaaaa")
+        dd.addLeading(image: UIImage(systemName: "human") ?? UIImage(), text: "sasdasdy")
+        ee.addLeading(image: UIImage(systemName: "sun") ?? UIImage(), text: "adsrhdh")
+        
+        let stackDaysForecast = UIStackView(arrangedSubviews: [aa, bb, cc, dd, ee])
+        stackDaysForecast.axis = .horizontal
+        stackDaysForecast.alignment = .fill
+        stackDaysForecast.distribution = .fillProportionally
+        stackDaysForecast.spacing = 5
+        
+        
+        scrollView.addSubview(stackDaysForecast)
+        daysForecast.addSubview(scrollView)
+                
+        stackDaysForecast.anchr(top: daysForecast.topAnchor, bottom: daysForecast.bottomAnchor, paddingTop: 2, paddingLeft: 2, paddingBottom: 2, paddingRight: 2)//, width: 555, height: 55)
+        scrollView.anchr(top: daysForecast.topAnchor, left: daysForecast.leftAnchor, bottom: daysForecast.bottomAnchor, right: daysForecast.rightAnchor)//, paddingTop: 2, paddingLeft: 2, paddingBottom: 2, paddingRight: 2, width: 0, height: 0)
+        
+        //scrollView.contentSize = CGSize(width: scrollView.bounds.width + 222, height: scrollView.bounds.height)
+        scrollView.showsHorizontalScrollIndicator = true
+        scrollView.backgroundColor = .cyan
+        //scrollView.centerX(stackDaysForecast)
+        daysForecast.isUserInteractionEnabled = true
+        
+        return daysForecast
+    }()
+    
+    
+}
--- WeatherNow/Modules/Main/Views/MainViewController.swift
+++ WeatherNow/Modules/Main/Views/MainViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    private let headView = HeadView()
+    
     //MARK: - view lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(headView)
         configureUI()
     }
     
@@ -20,119 +23,7 @@ class MainViewController: UIViewController {
         reconfigureUI()
     }
     
-    //MARK: - head container elements
-    private let isExpanded = true
-    
-    private lazy var headView: UIView = {
-        let hv = UIView()
-        hv.backgroundColor = .green
-        return hv
-    }()
-    
-    private let cityLabel: UILabelWithPadding = {
-        let lblc = UILabelWithPadding()
-        lblc.text = "New York"
-        lblc.font = lblc.font.withSize(28)
-        lblc.setSize(width: 266, height: 44)
-        lblc.backgroundColor = .brown
-        return lblc
-    }()
-    
-    private let locationMenuButton: UIButton = {
-        let button = UIButton()
-        button.setImage(UIImage(systemName: "line.3.horizontal")?
-                            .withTintColor(.green, renderingMode: .alwaysOriginal)
-                            .resized(to: CGSize(width: 25, height: 25)), for: .normal)
-        button.setImage(UIImage(systemName: "line.3.horizontal.decrease")?
-                            .withTintColor(.green, renderingMode: .alwaysOriginal)
-                            .resized(to: CGSize(width: 25, height: 25)), for: UIControl.State.highlighted)
-        button.addTarget(self, action: #selector(showLocationMenuController), for: .touchUpInside)
-        
-        return button
-    }()
-    
-    private let temperatureLabel: UILabelWithPadding = {
-        let lblt = UILabelWithPadding()
-        lblt.text = "+25k"
-        lblt.font = lblt.font.withSize(33)
-        lblt.setSize(width: 111, height: 44)
-        return lblt
-    }()
-    
-    private let cloudyImageContainer: UIImageView = {
-        var view = UIImageView()
-        let img = UIImage(systemName: "cloud.drizzle.fill")!
-            .withTintColor(.white, renderingMode: .alwaysOriginal)
-            .resized(to: CGSize(width: 66, height: 66))
-        let imgView = UIImageView(image: img)
-        view.addSubview(imgView)
-        return view
-    }()
-    
-    
-    //MARK: - Wind Pressure Humidity Container
-    private let windPressureHumidityContainer: UIView = {
-        let wphc = UIView()
-        
-        let wind = UILabel()
-        let pressure = UILabel()
-        let humid = UILabel()
-        
-        wind.addLeading(image: UIImage(systemName: "wind") ?? UIImage(), text: "10 m/s, NW")
-        pressure.addLeading(image: UIImage(systemName: "timer") ?? UIImage(), text: "701 mPa")
-        humid.addLeading(image: UIImage(systemName: "humidity") ?? UIImage(), text: "77%")
-        
-        let stackV1 = UIStackView(arrangedSubviews: [wind, pressure, humid])
-        stackV1.axis = .vertical
-        stackV1.distribution = .fillProportionally
-        stackV1.spacing = 17
-        stackV1.backgroundColor = .systemBlue
-        
-        wphc.addSubview(stackV1)
-        stackV1.anchr(top: wphc.topAnchor,
-                      paddingTop: 0,
-                      paddingLeft: 45,
-                      paddingRight: 25)
-        return wphc
-    }()
-    
-    //MARK: - Days Forecast Container
-    private let daysForecastContainer: UIView = {
-        let scrollView = UIScrollView()
-        let daysForecast = UIView()
-        
-        let aa = UILabel()
-        let bb = UILabel()
-        let cc = UILabel()
-        let dd = UILabel()
-        let ee = UILabel()
-                
-        aa.addLeading(image: UIImage(systemName: "car") ?? UIImage(), text: "Maonda")
-        bb.addLeading(image: UIImage(systemName: "bus") ?? UIImage(), text: "wednes")
-        cc.addLeading(image: UIImage(systemName: "humidity") ?? UIImage(), text: "sunDay")
-        dd.addLeading(image: UIImage(systemName: "human") ?? UIImage(), text: "sasdasdy")
-        ee.addLeading(image: UIImage(systemName: "sun") ?? UIImage(), text: "adsrhdh")
-        
-        let stackDaysForecast = UIStackView(arrangedSubviews: [aa, bb, cc, dd, ee])
-        stackDaysForecast.axis = .horizontal
-        stackDaysForecast.alignment = .fill
-        stackDaysForecast.distribution = .fillProportionally
-        stackDaysForecast.spacing = 5
-        
-        
-        scrollView.addSubview(stackDaysForecast)
-        daysForecast.addSubview(scrollView)
-                
-        stackDaysForecast.anchr(top: daysForecast.topAnchor, bottom: daysForecast.bottomAnchor, paddingTop: 2, paddingLeft: 2, paddingBottom: 2, paddingRight: 2)//, width: 555, height: 55)
-        scrollView.anchr(top: daysForecast.topAnchor, left: daysForecast.leftAnchor, bottom: daysForecast.bottomAnchor, right: daysForecast.rightAnchor)//, paddingTop: 2, paddingLeft: 2, paddingBottom: 2, paddingRight: 2, width: 0, height: 0)
-        
-        scrollView.contentSize = CGSize(width: scrollView.bounds.width + 222, height: scrollView.bounds.height)
-        scrollView.showsHorizontalScrollIndicator = true
-        scrollView.backgroundColor = .cyan
-        
-        return daysForecast
-    }()
-    
+  
 //MARK: - lower table
 
 var days =     ["Monday", "Tuesday", "Friday", "Sunday", "Monday", "Tuesday", "Friday", "Sunday"]
@@ -181,13 +72,13 @@ private extension MainViewController {
     func configureUI() {
         print("configuring UI")
         
-        let stackH1 = UIStackView(arrangedSubviews: [cityLabel, locationMenuButton])
+        let stackH1 = UIStackView(arrangedSubviews: [headView.cityLabel, headView.locationMenuButton])
         stackH1.axis = .horizontal
         stackH1.distribution = .equalSpacing
         stackH1.spacing = 17
         stackH1.backgroundColor = .systemBlue
         
-        let stackH2 = UIStackView(arrangedSubviews: [temperatureLabel, cloudyImageContainer])
+        let stackH2 = UIStackView(arrangedSubviews: [headView.temperatureLabel, headView.cloudyImageContainer])
         stackH2.axis = .horizontal
         stackH2.distribution = .equalCentering
         stackH2.spacing = 22
@@ -196,8 +87,8 @@ private extension MainViewController {
         
         headView.addSubview(stackH1)
         headView.addSubview(stackH2)
-        headView.addSubview(windPressureHumidityContainer)
-        headView.addSubview(daysForecastContainer)
+        headView.addSubview(headView.windPressureHumidityContainer)
+        headView.addSubview(headView.daysForecastContainer)
 
         
         stackH1.anchr(top: headView.topAnchor,
@@ -211,17 +102,17 @@ private extension MainViewController {
                       paddingLeft: 45,
                       paddingRight: 25)
         headView.insetsLayoutMarginsFromSafeArea = true
-        windPressureHumidityContainer.anchr(top: stackH2.bottomAnchor, left: stackH2.leftAnchor,
+        headView.windPressureHumidityContainer.anchr(top: stackH2.bottomAnchor, left: stackH2.leftAnchor,
                                             paddingTop: 3,
                                             paddingLeft: 15,
                                             paddingRight: 5)
-        daysForecastContainer.anchr(top:stackH2.bottomAnchor, left: cloudyImageContainer.leftAnchor,
+        headView.daysForecastContainer.anchr(top:stackH2.bottomAnchor, left: headView.cloudyImageContainer.leftAnchor,
                            paddingTop: 21,
                            paddingLeft: 1,
                            paddingBottom: 1,
                            paddingRight: 1)
-        daysForecastContainer.backgroundColor = .red
-        daysForecastContainer.setSize(width: 332, height: 99)
+        headView.daysForecastContainer.backgroundColor = .red
+        headView.daysForecastContainer.setSize(width: 332, height: 99)
         
         view.addSubview(headView)
         headView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height - (self.view.bounds.height / 3 * 2) - 44)
